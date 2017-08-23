388a1ad64e61d27e457bc0dc2daea3de0f4b8e12
nry <267261246@qq.com>
2017-08-23 10:43:59 +0800

    update

--- leetcode/3_longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.cpp
+++ leetcode/3_longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.cpp
@@ -22,27 +22,30 @@ class Solution1
             int len = s.length();
             int i = 0;
             int j = 0;
+            // i is the start pos of substring
             for (i = 0; i < len; i++)
             {
                 unordered_map<char, int> s_i_map;
                 unordered_map<char, int>::iterator iter;
+                // j is the end pos of substring
                 for (j = i + 1; j < len; j++)
                 {
+                    // everytime you store the (j-1) element into map
+                    s_i_map[s[j - 1]] = j - 1;
                     iter = s_i_map.find(s[j]);
                     // has duplicate character
                     if (iter != s_i_map.end())
-                    {
-                        result = (result > j - i - 1) ? result : (j - i - 1);
                         break;
-                    }
-                    s_i_map[s[j]] = j;
                 }
+                // be careful about the num of map without duplicate char is (j-i)
+                result = (result > j - i) ? result : (j - i);
             }
             return result;
         }
 };
 
-// sliding window
+// myself optimize
+// solution1 update i by one step
 class Solution2
 {
     public:
@@ -52,72 +55,66 @@ class Solution2
             int len = s.length();
             int i = 0;
             int j = 0;
-            unordered_map<char, int> s_i_map;
-            unordered_map<char, int>::iterator iter;
-            while (i < len && j < len)
+            // i is the start pos of substring
+            while (i < len)
             {
-                iter = s_i_map.find(s[j]);
-                if (iter == s_i_map.end())
+                unordered_map<char, int> s_i_map;
+                unordered_map<char, int>::iterator iter;
+                int j_dup = 0;
+                // j is the end pos of substring
+                for (j = i + 1; j < len; j++)
                 {
-                    s_i_map[s[j]] = j;
-                    result = (result > j - i) ? result : (j - i);
-                    j++;
+                    // everytime you store the (j-1) element into map
+                    s_i_map[s[j - 1]] = j - 1;
+                    iter = s_i_map.find(s[j]);
+                    // has duplicate character
+                    if (iter != s_i_map.end())
+                    {
+                        // j_dup is the pos of duplicate character
+                        j_dup = iter->second;
+                        break;
+                    }
                 }
-                else
-                {
-                    s_i_map.erase(iter);
-                    i++;
-                }
+                // be careful about the num of map without duplicate char is (j-i)
+                result = (result > j - i) ? result : (j - i);
+                //i = (j_dup > i) ? (j_dup + 1) : (i + 1);
+                // more simple
+                i = max(j_dup, i) + 1;
             }
             return result;
         }
 };
 
-// I optimize solution2 by skip i to duplicate index + 1
 class Solution3
 {
     public:
         int lengthOfLongestSubstring(string s)
         {
             int result = 0;
-            int len = s.length();
-            int i = 0;
-            int j = 0;
-            unordered_map<char, int> s_i_map;
-            unordered_map<char, int>::iterator iter;
-            while (i < len && j < len)
-            {
-                iter = s_i_map.find(s[j]);
-                if (iter == s_i_map.end())
-                {
-                    s_i_map[s[j]] = j;
-                    result = (result > j - i) ? result : (j - i);
-                    j++;
-                }
-                else
-                {
-                    i = distance(s_i_map.begin(), iter) + 1;
-                    s_i_map.erase(iter);
-                }
-            }
             return result;
         }
 };
 
 // code in discuss
+// same as approach #3 in solution page
+// note now the interval is (i, j]
 class Solution4
 {
     public:
         int lengthOfLongestSubstring(string s)
         {
             vector<int> dict(256, -1);
-            int result = 0, start = -1;
-            for (int i = 0; i != s.length(); i++)
+            int result = 0, i = -1;
+            for (int j = 0; j != s.length(); j++)
             {
-                if (dict[s[i]] > start)
-                    start = dict[s[i]];
-                dict[s[i]] = i;
-                result = max(result, i - start);
+                // is more simple, but hard to understand
+//                if (dict[s[j]] > i)
+//                    i = dict[s[j]];
+                // this one is easy to understand
+                if (dict[s[j]] > -1)
+                    i = max(dict[s[j]], i);
+                dict[s[j]] = j;
+                result = max(result, j - i);
             }
             return result;
         }
@@ -126,40 +123,49 @@ class Solution4
 int main(int argc, char** argv)
 {
     string str1 = "pwwkew";
-    string str2 = "abcabcbb";
+    string str2 = "abba";
     string str3 = "aab";
+    string str4 = "";
     int result = 0;
 
-//    Solution1 s1;
-//    result = s1.lengthOfLongestSubstring(str1);
-//    cout << "s1 max len: " << result << ", ";
-//    result = s1.lengthOfLongestSubstring(str2);
-//    cout << result << ", ";
-//    result = s1.lengthOfLongestSubstring(str3);
-//    cout << result << endl;
-//
-//    Solution2 s2;
-//    result = s2.lengthOfLongestSubstring(str1);
-//    cout << "s2 max len: " << result << ", ";
-//    result = s2.lengthOfLongestSubstring(str2);
-//    cout << result << ", ";
-//    result = s2.lengthOfLongestSubstring(str3);
-//    cout << result << endl;
-//
-//    Solution3 s3;
-//    result = s3.lengthOfLongestSubstring(str1);
-//    cout << "s3 max len: " << result << ", ";
-//    result = s3.lengthOfLongestSubstring(str2);
-//    cout << result << ", ";
-//    result = s3.lengthOfLongestSubstring(str3);
-//    cout << result << endl;
+    Solution1 s1;
+    result = s1.lengthOfLongestSubstring(str1);
+    cout << "s1 max len: " << result << ", ";
+    result = s1.lengthOfLongestSubstring(str2);
+    cout << result << ", ";
+    result = s1.lengthOfLongestSubstring(str3);
+    cout << result << ", ";
+    result = s1.lengthOfLongestSubstring(str4);
+    cout << result << endl;
+
+    Solution2 s2;
+    result = s2.lengthOfLongestSubstring(str1);
+    cout << "s2 max len: " << result << ", ";
+    result = s2.lengthOfLongestSubstring(str2);
+    cout << result << ", ";
+    result = s2.lengthOfLongestSubstring(str3);
+    cout << result << ", ";
+    result = s2.lengthOfLongestSubstring(str4);
+    cout << result << endl;
+
+    Solution3 s3;
+    result = s3.lengthOfLongestSubstring(str1);
+    cout << "s3 max len: " << result << ", ";
+    result = s3.lengthOfLongestSubstring(str2);
+    cout << result << ", ";
+    result = s3.lengthOfLongestSubstring(str3);
+    cout << result << ", ";
+    result = s3.lengthOfLongestSubstring(str4);
+    cout << result << endl;
 
     Solution4 s4;
-//    result = s4.lengthOfLongestSubstring(str1);
-//    cout << "s4 max len: " << result << ", ";
-//    result = s4.lengthOfLongestSubstring(str2);
-//    cout << result << ", ";
+    result = s4.lengthOfLongestSubstring(str1);
+    cout << "s4 max len: " << result << ", ";
+    result = s4.lengthOfLongestSubstring(str2);
+    cout << result << ", ";
     result = s4.lengthOfLongestSubstring(str3);
+    cout << result << ", ";
+    result = s4.lengthOfLongestSubstring(str4);
     cout << result << endl;
     
     return 0;
