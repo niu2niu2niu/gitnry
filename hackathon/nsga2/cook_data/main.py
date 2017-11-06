59492bab886204e3c6aaf0099c971f2d9937e07f
nry <267261246@qq.com>
2017-11-06 20:04:36 +0800

    修改hackathon项目的配置读入

--- hackathon/nsga2/cook_data/main.py
+++ hackathon/nsga2/cook_data/main.py
@@ -4,18 +4,39 @@
 import sys
 import read_file
 
+def step_total(d_step_info, d_step_total):
+    length = len(d_step_total)
+    for step_info in d_step_info:
+        new_idx = step_info + length
+        d_step_total[new_idx] = d_step_info[step_info]
+        if 'restraint' not in d_step_total[new_idx]:
+            d_step_total[new_idx]['restraint'] = []
+        for i in range(len(d_step_total[new_idx]['restraint'])):
+            d_step_total[new_idx]['restraint'][i] += length
+
 if __name__ == '__main__':
     # 读取资源列表
     machine_list_filename = "machine_list.txt"
-    d_machine_list = read_file.read_machine_list(machine_list_filename)
-    print d_machine_list
+    (d_machine_list, d_machine_list_unfold) = read_file.read_machine_list(machine_list_filename)
+    print 'd_machine_list:', d_machine_list
+    print 'd_machine_list_unfold:', d_machine_list_unfold
     
     # 读当前需要调度的菜名
     dish_name_filename = "dish_name.txt"
     d_dish_name = read_file.read_dish_name(dish_name_filename)
-    print d_dish_name
+    print 'd_dish_name:', d_dish_name
 
     # 读取菜名和任务文件名的对应表
     dish_file_map_filename = "dish_file_map.txt"
     d_dish_file_map = read_file.read_dish_file_map(dish_file_map_filename)
-    print d_dish_file_map
+    print 'd_dish_file_map:', d_dish_file_map
+
+    # 根据每个菜对应的文件读取任务信息
+    d_step_total = {}
+    for dish_name in d_dish_name:
+        dish_filename = d_dish_file_map[dish_name]
+        print 'dish_filename:', dish_filename
+        d_step_info = read_file.read_dish_file(dish_filename)
+        print 'd_step_info:', d_step_info
+        step_total(d_step_info, d_step_total)
+        #print 'd_step_total', d_step_total
--- hackathon/nsga2/cook_data/read_file.py
+++ hackathon/nsga2/cook_data/read_file.py
@@ -18,13 +18,20 @@ def read_dish_name(filename):
 # 读取资源列表
 def read_machine_list(filename):
     d = {}
+    d_unfold = {}
+    count = 1
     with open(filename, 'r') as f:
         data = f.readlines()
         for line in data:
             line = line.strip()
             idx, name, num = line.split('\t')
+            num = int(num)
             d[idx] = (name, num)
-    return d
+            d_unfold[idx] = []
+            for i in range(num):
+                d_unfold[idx].append(count + i)
+            count += num
+    return (d, d_unfold)
 
 # 读取菜名和任务文件名的对应表
 def read_dish_file_map(filename):
@@ -38,4 +45,33 @@ def read_dish_file_map(filename):
     return d
 
 # 读取每个菜的任务文件
-
+def read_dish_file(filename):
+    d = {}
+    with open(filename, 'r') as f:
+        data = f.readlines()
+        for line in data:
+            line = line.strip()
+            # text
+            if line.count('\t') == 2:
+                flag, step, text = line.split('\t')
+                step = int(step.replace('step', ''))
+                d[step] = {'text':text}
+            # restraint
+            if line.count('\t') == 1:
+                flag, restraint = line.split('\t')
+                step_high, step_low = restraint.split('>')
+                step_high = int(step_high.replace('step', ''))
+                step_low = int(step_low.replace('step', ''))
+                if step_high in d:
+                    if 'restraint' not in d[step_high]:
+                        d[step_high]['restraint'] = [step_low]
+                    else:
+                        d[step_high]['restraint'].append(step_low)
+            # machine and time
+            if line.count('\t') == 3:
+                flag, step, machine, time = line.split('\t')
+                step = int(step.replace('step', ''))
+                time = int(time.replace('time=', ''))
+                d[step]['time'] = time
+    d_s = sorted(d.iteritems(), key=lambda d:d[0], reverse = True)
+    return d
